00dba685deb6ea31b95ed0ef0647e9c948763476
Manuel Moya <mmoyam@gmail.com>
2018-06-03 21:02:26 -0400

    fix "\r" window :(
    fix "\r" window :(

--- equivalencia.js
+++ equivalencia.js
@@ -41,6 +41,7 @@ var getTransitionsNewAFD =  function(simbolos, cola_estado_revisar, arr_clausura
 			// Element es la letra del alfabeto que se evaluará 
             //var symbol = element;
             var symbol = simbolos[l];
+            symbol= symbol.replace('\r', '');
             var arr_clausura_estado = [];
             //var arr_clausura_estado = matriz_afd_analisis[estado_cola][2];
 
@@ -80,8 +81,7 @@ var getTransitionsNewAFD =  function(simbolos, cola_estado_revisar, arr_clausura
             
 			// Si no encontró otra lista igual
 
-			symbol= symbol.replace(/\n$/, '');
-            if ( estado_existe == -1 ) {
+			if ( estado_existe == -1 ) {
             	//console.log("agregar nuevo: "+ arr_clausura_final.toString());
             	maxState++;
                 matriz_afd_analisis.push([estado_cola, symbol, arr_clausura_final, maxState]);
