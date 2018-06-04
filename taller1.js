902683ee87dc6ed56f47715956a6927cf5bc7451
Manuel Moya <mmoyam@gmail.com>
2018-06-03 20:11:03 -0400

    refactoring
    refactoring

--- taller1.js
+++ taller1.js
@@ -10,19 +10,8 @@ const fun_grales = require('./funciones_grales.js');
 const readFile = require('./readFile.js');
 
 
-/* lee archivo .txt*/
-
+/* lee datos entrada desde archivo .txt*/
 readFile.leer_archivo();
-/*
-readFile.read( function (err, result){
- 	// error
- 	if (err) {
-     	console.log("error:" + err);
-     	return;	
-    }
-    console.log("est: " + global.getNro_estados() );
-});
-*/
 
 /* ver valores leidos de archivo*/
 titulos.datos_entrada();
@@ -54,9 +43,7 @@ titulos.matriz_dfd_analisis();
 var matriz_clausura = global.getMatriz_clausura();
 var simbolos = global.getSimbolos();
 
-
 let queueNewStateAF = new Queue();
 let listNewAnalysis = equiv.getTransitionsNewAFD(simbolos,queueNewStateAF,matriz_clausura[0]);
 
 equiv.imprimir_matriz_analisis();
-
