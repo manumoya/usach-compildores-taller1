3112cf15a2083f4d436c99922ee9397863b553a3
Manuel Moya <mmoyam@gmail.com>
2018-06-03 20:02:44 -0400

    refactoring
    refactoring

--- taller1.js
+++ taller1.js
@@ -22,20 +22,6 @@ readFile.read( function (err, result){
 /* ver valores leidos de archivo*/
 titulos.datos_entrada();
 fun_grales.imprimir_datos_entrada();
-/*
-
-console.log("Estados : " + estados );
-console.log("Simbolos : " +  simbolos);
-console.log("Estado inicial : " + global.getEstado_inicial() );
-console.log("Estados finales: " + global.getEstado_finales() );
-arr_transiciones = global.getTransiciones();
-
-for (var i=0; i < arr_transiciones.length; i++){
-		console.log("transicion["+ i+"] " + arr_transiciones[i].orig +" / "+ 
-					arr_transiciones[i].simb +" / "+ arr_transiciones[i].final);
-}
-console.log("\n");
-*/
 
 /* llenado de matriz */
 titulos.automata_entrada();
@@ -43,7 +29,6 @@ matriz.crear();
 matriz.preparar();
 matriz.llenar();
 matriz.ver();
-//global.setMatriz(arr_matriz);
 
 /*  validar si es un AFD */
 titulos.validar_automata();
@@ -59,24 +44,14 @@ titulos.matriz_clausura();
 claus.definir_matriz_e();
 claus.imprimir_matriz();
 
-/* equivalencias */
+/* procesar equivalencias */
 titulos.matriz_dfd_analisis();
 var matriz_clausura = global.getMatriz_clausura();
 var simbolos = global.getSimbolos();
-//equiv.get_arreglo_transiciones(matriz_clausura[0],0);
 
-//claus.imprimir_matriz();
 
 let queueNewStateAF = new Queue();
 let listNewAnalysis = equiv.getTransitionsNewAFD(simbolos,queueNewStateAF,matriz_clausura[0]);
 
 equiv.imprimir_matriz_analisis();
-
-
-
-/*
-var arrPila =[];
-arrPila.push(0);
-autom.recorre_efnd(0, arrPila);
-*/
 
